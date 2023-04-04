de06acd474876a1607f249d9740de8138d1b26e9
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-04 20:53:08 -0300

    aula de java 4/4

--- 05-Encapsulamento/src/br/com/fiap/padaria/view/Terminal.java
+++ 05-Encapsulamento/src/br/com/fiap/padaria/view/Terminal.java
@@ -6,15 +6,29 @@ public class Terminal {
 
 	//Criar o método: main + CTRL + espaco
 	public static void main(String[] args) {
+		
 		//Instanciar um produto
 		Produto p = new Produto();
 		
 		//Ajustar para utilizar os métodos set
-		p.setNome("CD");
+		p.setNome("Livro Java");
 		p.setId(5);
+		p.setPrecoCusto(50);
+		p.setPrecoVenda(150);
+		p.setPromocao(true);
 		
 		
 		//Exibir os atributos do produto
+		//Id, nome, preco custo, preco venda, promocao, qtd
+		System.out.println(p.getId() + " " + 
+		    " " + p.getNome() +
+		    " " + p.getPrecoCusto() +
+		    " " + p.getPrecoVenda() +
+		    " " + p.isPromocao() +
+		    " " + p.getQuantidade());
+		
+		
+
 		
 		
 	}
---
+++ 05-Encapsulamento/src/cor/Cor.java
@@ -0,0 +1,49 @@
+package cor;
+
+public class Cor {
+
+	private int r;
+	private int g;
+	private int b;
+	private String nome;
+	
+	public void alterarCor(int r, int g, int b, String nome) {
+		this.r = r;
+		this.g = g;
+		this.b = b;
+		this.nome = nome;
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public void setR(int r) {
+		this.r = r;
+	}
+
+	public int getG() {
+		return g;
+	}
+
+	public void setG(int g) {
+		this.g = g;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
